36d1012c744ff8f808ad1c7539a0e692a94f624e
Trevor Anderson <trevoranderson1@hotmail.com>
2019-04-25 12:00:25 -0700

    delete

--- client/src/tests/components/Navbar.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Navbar from '../../components/Navbar';
-
-it('should render Navbar', () => {
-  const wrapper = shallow(<Navbar logoutUser={jest.fn()} user={{}} />);
-  expect(wrapper).toMatchSnapshot();
-});
--- client/src/tests/components/NavbarLeftMenu.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import NavbarLeftMenu from '../../components/NavbarLeftMenu';
-
-it('should render NavbarLeftMenu', () => {
-  const wrapper = shallow(<NavbarLeftMenu user={{}} />);
-  expect(wrapper).toMatchSnapshot();
-});
